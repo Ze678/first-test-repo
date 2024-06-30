96b7dfae1211724eb2bdb28771fd66e3916db6e0
Ze678 <zee123hmz@gmail.com>
2024-06-30 10:42:51 +0530

    Test2

--- exercise.py
+++ exercise.py
@@ -6,7 +6,11 @@
 
 val = int(input("Enter a number: "))
 
-for x in range(val):
-    for y in range(val):
-        print('*', end='')
-    print('')  # Move to the next line after printing each row of the square
+# for x in range(val):
+#     for y in range(val):
+#         print('*', end='')
+#     print('')  # Move to the next line after printing each row of the square
+
+print("Just a test to see if the test works")
+
+
